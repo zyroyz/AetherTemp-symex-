2474b859269d17f4159e3b768678733491484afa
zyro <126006401+zyroyz@users.noreply.github.com>
2025-03-22 18:23:48 +0000

    Add files via upload

---
+++ AetherTemp/Menu/Settings.cs
@@ -0,0 +1,33 @@
+﻿using StupidTemplate.Classes;
+using UnityEngine;
+using static StupidTemplate.Menu.Main;
+
+namespace StupidTemplate
+{
+    internal class Settings
+    {
+        public static ExtGradient backgroundColor = new ExtGradient { isRainbow = false };
+        public static ExtGradient[] buttonColors = new ExtGradient[]
+        {
+            new ExtGradient{colors = GetSolidGradient(new Color(42f / 255f, 29f / 255f, 119f / 255f)) }, // Disabled
+            new ExtGradient{isRainbow = false} // Enabled
+        };
+        public static Color[] textColors = new Color[]
+        {
+            Color.white, // Disabled
+            Color.white // Enabled
+        };
+
+        public static Font currentFont = (Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font);
+
+        public static bool fpsCounter = false;
+        public static bool disconnectButton = false;
+        public static bool rightHanded = false;
+        public static bool disableNotifications = false;
+
+        public static KeyCode keyboardButton = KeyCode.Q;
+
+        public static Vector3 menuSize = new Vector3(0.1f, 1f, 0.97f); // Depth, Width, Height
+        public static int buttonsPerPage = 7;
+    }
+}
